6016d208ac6cdc3e8806c4a4a9e64b1674aaad6f
Audrone S. <audra.mvc@gmail.com>
2024-02-01 09:32:32 -0500

    About me task

--- src/java_day_01_practice/AboutMe.java
+++ src/java_day_01_practice/AboutMe.java
@@ -4,6 +4,7 @@ public class AboutMe {
 
     public static void main(String[] args) {
 
+        // Print out lines in console
         System.out.println("5.1 Your Favorite book: \"Reading electronic books\"" +
                 "\n5.2 Your favorite quote: \"The purpose of our lives is to be happy\"" +
                 "\n5.3 Your favorite TV show: \"The charmed\"" +
