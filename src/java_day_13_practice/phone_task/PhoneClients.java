1ad4c9b0e48c27be8d53ed522db3a504391d8d8a
Audrone S. <audra.mvc@gmail.com>
2024-02-13 14:00:49 -0500

    Day 13 task Phone

--- src/java_day_13_practice/phone_task/PhoneClients.java
+++ src/java_day_13_practice/phone_task/PhoneClients.java
@@ -5,13 +5,17 @@ public class PhoneClients {
 
         Iphone iphone = new Iphone("Apple","MaxPro", "528", 1_500, "Silver");
         Samsung samsung = new Samsung("Galaxy", "flip5", "128", 1_000, "Black");
+        Nokia nokia = new Nokia("Nokia", "3320", "258", 500, "White");
 
         System.out.println(iphone);
         System.out.println(samsung);
+        System.out.println(nokia);
 
         iphone.text(iphone.phoneNumber);
         iphone.call(iphone.phoneNumber);
         samsung.faceTime(samsung.phoneNumber);
         samsung.text(samsung.phoneNumber);
+        nokia.call(nokia.phoneNumber);
+        nokia.text(nokia.phoneNumber);
     }
 }
